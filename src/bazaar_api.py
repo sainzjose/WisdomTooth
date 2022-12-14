91e132d60af42e2312ad4d9de97dd6e686016804
andrewfritz2001 <andrew_fritz@outlook.com>
2022-12-14 01:23:18 -0700

    basic api functionality

---
+++ src/bazaar_api.py
@@ -0,0 +1,58 @@
+import requests
+import json 
+
+""" Every sample in MalwareBazaar container the following fieds
+
+1. Date        (date uploaded)
+2. SHA256 Hash (unique identifier)
+3. Type        (exe, elf, dll)
+4. Signature   (malware signature, sometimes None)
+5. Tag         (type, malware type, targetted architecture, etc...)
+6. Report      (user who submitted sample)
+
+"""
+
+class MalwareBazaar:
+
+    @staticmethod
+    def get_samples_by_tag(file_type,number):
+        """ Get X number of samples with a file type of Y. Returns a list of hashes.
+
+        Only 1000 samples can be queryed at once
+        Request will timeout in 15 seconds
+        
+        """
+
+        if isinstance(file_type,str) is False or isinstance(number,str) is False:
+            raise Exception("Invalid input type. Both inputs must be a string")
+        data = {
+            'query':'get_file_type',
+            'file_type' :file_type,
+            'limit':number,
+        }
+        response = requests.post('https://mb-api.abuse.ch/api/v1/', data=data, timeout=25)
+        json_response = json.loads(response.text)
+        return [item['sha256_hash'] for item in json_response['data']]
+
+    @staticmethod
+    def download_files(hashes):
+        """ Expects a list of hashes. Returns nothing. 
+
+        Downloads all of the files associated with the hashes. 
+        
+        """
+
+        if isinstance(hashes,list) is False:
+            raise Exception("Invalid input type. Input must be a list of hashes represented with a string")
+        for hash in hashes:
+            data = {
+                'query':'get_file',
+                'sha256_hash':hash
+            }
+            response = requests.post('https://mb-api.abuse.ch/api/v1/', data=data, timeout=100, allow_redirects=True)
+            open(f"../bin/malicious/{hash}", 'wb').write(response.content)
+
+
+# How to download 5 malicious elf files
+hashes = MalwareBazaar.get_samples_by_tag('elf','5')
+MalwareBazaar.download_files(hashes)
